6f43dd9d551d317f2ec44ed543b3dd6470ff3331
Rsiddique98 <ridasiddique@yahoo.com>
2025-02-27 22:09:08 +0000

    Added navigation links to Home, create and edit pages under header.ejs

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+// routes 
+app.get("/", (req, res) => {
+    res.render('index.ejs');
+});
+
+app.get("/create", (req, res) => {
+    res.render('create.ejs');
+});
+
+app.get("/edit", (req, res) => {
+    res.render('edit.ejs');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+})
