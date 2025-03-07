3fc5f6a4928d8fc93f6ff3c3067b1a9927b9c263
Rsiddique98 <ridasiddique@yahoo.com>
2025-03-07 05:27:26 +0000

    Fixed static file handling

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 });
 
 app.set('view engine', 'ejs');
-app.use(express.static("public"));
+app.use(express.static(path.join(path.resolve(), "public")));
 app.use(bodyParser.urlencoded({extended: true }));
 
 
